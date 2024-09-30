e42631b3ff9506ab99f8ba3c3331d9cbc4348bd8
gabriel-skinny <gabrielcatoni12@gmail.com>
2024-09-30 09:00:43 -0300

    Finishign replaceStream example

--- 6 - Streams/code/Transform/replaceStream.js
+++ 6 - Streams/code/Transform/replaceStream.js
@@ -14,5 +14,28 @@ export class ReplaceStream extends Transform {
     const tailLen = this.searchStr.length - 1;
     this.tail = lastPiece.slice(-tailLen);
     pieces[pieces.length - 1] = lastPiece.slice(0, -tailLen);
+    this.push(pieces.join(this.replaceStr));
+    callback();
+  }
+
+  _flush(callback) {
+    this.push(this.tail);
+    callback();
   }
 }
+
+const replaceStream = new ReplaceStream("World", "Node.js");
+
+replaceStream.on("data", (chunk) =>
+  console.log(`Chunk: ${chunk.toString()}\n`)
+);
+
+replaceStream.write("Hello W");
+const data = replaceStream.read();
+replaceStream.write("o");
+replaceStream.write("r");
+replaceStream.write("ld!");
+const data2 = replaceStream.read();
+replaceStream.end();
+
+console.log({ data, data2 });
